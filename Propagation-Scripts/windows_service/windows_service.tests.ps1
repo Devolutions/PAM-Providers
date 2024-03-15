add8b3cdf40859256ed0012850393b2047c69ab6
Adam Bertram <adbertram@gmail.com>
2024-03-15 10:07:33 -0500

    updated tests

--- Propagation-Scripts/windows_service/windows_service.tests.ps1
+++ Propagation-Scripts/windows_service/windows_service.tests.ps1
@@ -29,33 +29,39 @@ Describe 'Windows Service propagation script' {
             label         = 'one specific service to restart'
             parameter_set = ($mandatoryParameters + @{
                     ServiceName    = 'service1'
-                    RestartService = $true
+                    RestartService = 'yes'
                 })
         }
         @{
             label         = 'multiple specific services to restart'
             parameter_set = ($mandatoryParameters + @{
                     ServiceName    = 'service1,service2'
-                    RestartService = $true
+                    RestartService = 'yes'
                 })
         }
         @{
             label         = 'all services to restart'
             parameter_set = ($mandatoryParameters + @{
-                    RestartService = $true
+                    RestartService = 'yes'
                 })
         }
     )
 
     It 'has a README doc' {
-        Test-Path "$PSScriptRoot\README.md" | Should -BeTrue
+        "$PSScriptRoot\README.md" | Should -Exist
     }
 
     It 'has a prerequisites test script' {
-        Test-Path "$PSScriptRoot\prerequisites.tests.ps1" | Should -BeTrue
+        "$PSScriptRoot\prerequisites.tests.ps1" | Should -Exist
+    }
+
+    It 'has a DVLS template created' {
+        "$PSScriptRoot\template.json" | Should -Exist
     }
 
     Context 'when no services running the the specified username exist' {
+
+        $ctxParameterSets = $parameterSets.where({ !$_.parameter_set.ContainsKey('ServiceName') })
 
         BeforeAll {
             Mock 'Write-Output'
@@ -72,40 +78,9 @@ Describe 'Windows Service propagation script' {
             }
         }
 
-        It 'throws the expected error message : <_.label>' -ForEach $parameterSets {
+        It 'throws the expected error message : <_.label>' -ForEach $ctxParameterSets {
 
             { & "$PSScriptRoot\script.ps1" @parameter_set } | Should -Throw '*No services found on * running as * could be found*'
-
-        }
-
-    }
-
-    Context 'when at least one specific service could not be found' {
-
-        $ctxParameterSets = $parameterSets.where({ $_.parameter_set.ContainsKey('ServiceName') })
-
-        BeforeAll {
-            Mock 'Write-Output'
-            Mock Invoke-Command {
-                $functionsToDefine = @{
-                    'Get-CimInstance' = {
-                        [pscustomobject]@{
-                            Name      = 'nomatchingservicename'
-                            StartName = 'somerandomuserhere'
-                        }
-                    }
-                }
-    
-                # Define mock variables if necessary
-                $variablesToDefine = New-Object System.Collections.Generic.List[System.Management.Automation.PSVariable]
-    
-                # Invoke the script block with mock context and arguments
-                $ScriptBlock.InvokeWithContext($functionsToDefine, $variablesToDefine, $ArgumentList)
-            }
-        }
-
-        It 'throws the expected error message : <_.label>' -ForEach $ctxParameterSets {
-            { & "$PSScriptRoot\script.ps1" @parameter_set } | Should -Throw '*The following services could not be found on host*'
 
         }
 
@@ -188,42 +163,43 @@ Describe 'Windows Service propagation script' {
         }
     }
 
-    # context 'when applicable services are found and not running' {
+    Context 'when applicable services are found and not running' {
 
-    #     BeforeAll {
-    #         mock 'Write-Output'
-    #         Mock Invoke-Command {
-    #             $functionsToDefine = @{
-    #                 'Get-CimInstance'       = {
-    #                     [pscustomobject]@{
-    #                         Name = 'service1'
-    #                         StartName = 'serviceuserhere'
-    #                         State = 'Stopped'
-    #                     }
-    #                     [pscustomobject]@{
-    #                         Name = 'service2'
-    #                         StartName = 'serviceuserhere'
-    #                         State = 'Stopped'
-    #                     }
-    #                 }
-    #             }
+        BeforeAll {
+            Mock 'Write-Output'
+            Mock Invoke-Command {
+                $functionsToDefine = @{
+                    'Get-CimInstance' = {
+                        [pscustomobject]@{
+                            Name      = 'service1'
+                            StartName = 'serviceuserhere'
+                            State     = 'Stopped'
+                        }
+                        [pscustomobject]@{
+                            Name      = 'service2'
+                            StartName = 'serviceuserhere'
+                            State     = 'Stopped'
+                        }
+                    }
+                    'Invoke-CimMethod' = {
+                        [pscustomobject]@{
+                            ReturnValue = 0
+                        }
+                    }
+                }
     
-    #             # Define mock variables if necessary
-    #             $variablesToDefine = New-Object System.Collections.Generic.List[System.Management.Automation.PSVariable]
+                # Define mock variables if necessary
+                $variablesToDefine = New-Object System.Collections.Generic.List[System.Management.Automation.PSVariable]
     
-    #             # Invoke the script block with mock context and arguments
-    #             $ScriptBlock.InvokeWithContext($functionsToDefine, $variablesToDefine, $ArgumentList)
-    #         }
-    #     }
-        
+                # Invoke the script block with mock context and arguments
+                $ScriptBlock.InvokeWithContext($functionsToDefine, $variablesToDefine, $ArgumentList)
+            }
+        }
 
-    # }
 
-    # context 'when an applicable service is not running' {
+        It 'returns true : <_.label>' -ForEach $parameterSets {
 
-    # }
-
-    # context 'when the update password method fails' {
-
-    # }
+            & "$PSScriptRoot\script.ps1" @parameter_set | Should -BeTrue
+        }
+    }
 }
